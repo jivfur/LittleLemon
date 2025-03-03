9c47bdcd6265d7556d8cf7232bd3bb439bc63b28
Ivan Fuentes <jivfur@gmail.com>
2025-03-03 15:36:58 -0500

    Fix errors in Model, Migrate

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Booking(models.Model):
     name = models.CharField(max_length=255)
-    no_of_guests = models.IntegerField(max_length=6)
+    no_of_guests = models.IntegerField()
     booking_date = models.DateTimeField(default=10)
     
     def __str__(self):
@@ -12,6 +12,6 @@ class Booking(models.Model):
 class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    inventory = models.IntegerField(max_length=5)
+    inventory = models.IntegerField()
     def __str__(self):
         return str(self.title)
