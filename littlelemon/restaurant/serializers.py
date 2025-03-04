e83673854f6974abc7bf6e1b90d0f9521c805478
Ivan Fuentes <jivfur@gmail.com>
2025-03-03 21:19:46 -0500

    Add Views for MenuItem and update endpoints

--- littlelemon/restaurant/serializers.py
+++ littlelemon/restaurant/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Menu, Booking
 
@@ -10,3 +11,8 @@ class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
         fields = "__all__"
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+from rest_framework.generics import ListCreateView,RetrieveUpdateAPIView,DestroyAPIView
+from rest_framework.response import Response
+
+from .models import Menu
+from .serializers import MenuSerializer
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(ListCreateView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(RetrieveUpdateAPIView,DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
